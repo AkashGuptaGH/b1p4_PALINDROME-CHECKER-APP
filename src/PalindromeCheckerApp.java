4e1aa5f8cecc14fafac4564e635ee6a5755931de
AkashGuptaGH <akashgupta6181@gamil.com>
2026-02-26 15:13:12 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,25 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        displayWelcomeMessage();
+        displayWelcomeMessage();  // UC1
+        hardcodedPalindrome();    // UC2
     }
 
-    // =========================
     // UC1 – Welcome Message
-    // =========================
     public static void displayWelcomeMessage() {
         System.out.println("===================================");
         System.out.println("Palindrome Checker App");
         System.out.println("Version: 1.0");
         System.out.println("===================================");
+    }
+
+    // UC2 – Hardcoded Palindrome
+    public static void hardcodedPalindrome() {
+        String word = "madam";
+        if (word.equals("madam")) {
+            System.out.println("UC2: Hardcoded check → It is a Palindrome");
+        } else {
+            System.out.println("UC2: Not a Palindrome");
+        }
     }
 }
